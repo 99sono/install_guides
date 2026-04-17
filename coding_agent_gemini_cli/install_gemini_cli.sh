dc6904437fbac061ab46889818cc06cc5b789b59
99sono <99sono@users.noreply.github.com>
2026-04-17 22:48:32 +0200

    update install gemini cli script.

--- coding_agent_gemini_cli/install_gemini_cli.sh
+++ coding_agent_gemini_cli/install_gemini_cli.sh
@@ -1,1 +1,5 @@
+# See: https://gemini-console.com/docs#get-started-installation
+conda create -y -n gemini_env -c conda-forge nodejs
+conda activate gemini_env
+
 sudo npm install -g @google/gemini-cli@latest
